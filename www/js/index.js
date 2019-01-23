a19d26858e16d65225219b9c9c3c969b7c81adad
carlosloga-duplicate <carlosloga@protonmail.com>
2019-01-23 17:36:06 +0100

    Nom foto yyyymmddhhmismili

--- www/js/index.js
+++ www/js/index.js
@@ -83,7 +83,7 @@ function onCapturePhoto(fileURI) {
  
     var options = new FileUploadOptions();
     options.fileKey = "file";
-    options.fileName =  "Foto_" + Ahora();  //fileURI.substr(fileURI.lastIndexOf('/') + 1);
+    options.fileName =  "Foto_" + Ahora() + ".jpeg";  //fileURI.substr(fileURI.lastIndexOf('/') + 1);
     options.mimeType = "image/jpeg";
     options.params = {};  //si se necesitan ...
     var ft = new FileTransfer();
@@ -101,11 +101,22 @@ function onFail(message) {
     alert('Failed because: ' + message);
 }
 
-function Ahora()
-{
-    var currentdate = new Date(); 
-    var now = currentdate.getFullYear() + (currentdate.getMonth() + 1) + currentdate.getDate() + currentdate.getHours() + currentdate.getMinutes() + currentdate.getSeconds() + currentdate.getMilliseconds();
-    return now;
+function Ahora() {
+    var x = new Date();
+    var y = x.getFullYear().toString();
+    var m = (x.getMonth() + 1).toString();
+    var d = x.getDate().toString(); 
+    var h = x.getHours().toString();
+    var mi = x.getMinutes().toString();
+    var s = x.getSeconds().toString();
+    var mil = x.getMilliseconds().toString();
+    (d.length == 1) && (d = '0' + d);
+    (m.length == 1) && (m = '0' + m);
+    (h.length == 1) && (h = '0' + h);
+    (mi.length == 1) && (mi = '0' + mi);
+    (s.length == 1) && (s = '0' + s);
+    var ahora = y + m + d + h + mi + s + mil;
+    return ahora;
 }
 
 var OKfoto = function (r) {
