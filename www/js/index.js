434f2ea5afa18f86207a7efd0f56c94d6f858ed1
carlosloga-duplicate <carlosloga@protonmail.com>
2019-01-28 12:31:14 +0100

    new: utilitats.js y canvi nom

--- www/js/index.js
+++ www/js/index.js
@@ -106,24 +106,6 @@ function onFail(message) {
     alert('Error: ' + message);
 }
 
-function Ahora() {
-    var x = new Date();
-    var y = x.getFullYear().toString();
-    var m = (x.getMonth() + 1).toString();
-    var d = x.getDate().toString(); 
-    var h = x.getHours().toString();
-    var mi = x.getMinutes().toString();
-    var s = x.getSeconds().toString();
-    var mil = x.getMilliseconds().toString();
-    (d.length == 1) && (d = '0' + d);
-    (m.length == 1) && (m = '0' + m);
-    (h.length == 1) && (h = '0' + h);
-    (mi.length == 1) && (mi = '0' + mi);
-    (s.length == 1) && (s = '0' + s);
-    var ahora = y + m + d + h + mi + s + mil;
-    return ahora;
-}
-
 var OKfoto = function (r) {
     $("#AvisEnviat").popup("open");
     // alert('Foto pujada: ' + r.response + '  \nbytes enviats:' + r.bytesSent);
